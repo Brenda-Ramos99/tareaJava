266fd4a6bac531b75ae32546e136ae7ef7869181
ElvisGomez <49737911+ElvisGomez@users.noreply.github.com>
2019-08-17 20:56:59 -0600

    Delete sentenciasSqlQUERYs.sql

--- sentenciasSqlQUERYs.sql
+++
@@ -1,7 +0,0 @@
-use AlfaroRecinosAyalaVillatoroRamos;
-select * from facultad;
-select * from carrera;
-select * from estudiante;
-select * from usuario;
-select * from tipousuario;
-insert into facultad (nombre,telefono) values ("Facultad de Ciencias y Humanidades", "2292-4565");
