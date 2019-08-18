072d990b8384905b523fd6781c8c6b891e5dbee6
ElvisGomez <49737911+ElvisGomez@users.noreply.github.com>
2019-08-17 18:38:02 -0600

    Add files via upload

---
+++ sentenciasSqlQUERYs.sql
@@ -0,0 +1,7 @@
+use AlfaroRecinosAyalaVillatoroRamos;
+select * from facultad;
+select * from carrera;
+select * from estudiante;
+select * from usuario;
+select * from tipousuario;
+insert into facultad (nombre,telefono) values ("Facultad de Ciencias y Humanidades", "2292-4565");
